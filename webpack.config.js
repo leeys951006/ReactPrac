51a19ccf88cd044ddae8881deb7ca6f5dad53a11
이연승 <leeys951006@gmail.com>
2024-07-09 10:12:58 +0900

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: '',
+  output: {
+    file: '',
+    path: '',
+  },
+  module: {
+    rules: [],
+  },
+  resolve: {
+    extensions: [],
+  },
+};
